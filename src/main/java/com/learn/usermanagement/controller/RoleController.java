0a2e47595abd5a5154f34a2cdfb38a8cd966be89
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-28 04:02:33 -0700

    add class RoleController

--- src/main/java/com/learn/usermanagement/controller/RoleController.java
+++ src/main/java/com/learn/usermanagement/controller/RoleController.java
@@ -3,10 +3,9 @@ package com.learn.usermanagement.controller;
 import com.learn.usermanagement.model.Role;
 import com.learn.usermanagement.repository.RoleRepository;
 import com.learn.usermanagement.service.RoleService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("Role")
@@ -22,5 +21,10 @@ public class RoleController {
     @PostMapping
     public Role save(@RequestBody Role role) {
         return repository.save(role);
+    }
+
+    @GetMapping
+    public List<Role> roleList() {
+        return repository.findAll();
     }
 }
