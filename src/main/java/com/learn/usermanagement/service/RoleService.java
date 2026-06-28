f0c67194d094bfe788a09efb6169492d5f9f9b49
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-28 03:07:21 -0700

    add class RoleService

--- src/main/java/com/learn/usermanagement/service/RoleService.java
+++ src/main/java/com/learn/usermanagement/service/RoleService.java
@@ -1,7 +1,10 @@
 package com.learn.usermanagement.service;
 
+import com.learn.usermanagement.model.Role;
 import com.learn.usermanagement.repository.RoleRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class RoleService {
@@ -9,5 +12,14 @@ public class RoleService {
 
     public RoleService(RoleRepository repository) {
         this.repository = repository;
+    }
+
+    public Role update(Long id, Role newData) {
+        Optional<Role> roleOpt = repository.findById(id);
+
+        Role role = roleOpt.get();
+
+
+
     }
 }
