46b8ab83b0bd98def0259d4d90f11f29d13fe48a
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-28 02:35:09 -0700

    add class user

---
+++ src/main/java/com/learn/usermanagement/model/User.java
@@ -0,0 +1,15 @@
+package com.learn.usermanagement.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+}
