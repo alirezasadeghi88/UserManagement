79f4617618f49d051b26b8834e4ca2e5ce9d52e1
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-28 03:52:11 -0700

    add class UserController

--- src/main/java/com/learn/usermanagement/controller/UserController.java
+++ src/main/java/com/learn/usermanagement/controller/UserController.java
@@ -1,7 +1,10 @@
 package com.learn.usermanagement.controller;
 
+import com.learn.usermanagement.model.User;
 import com.learn.usermanagement.repository.UserRepository;
 import com.learn.usermanagement.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +17,10 @@ public class UserController {
     public UserController(UserRepository userRepository, UserService service) {
         this.userRepository = userRepository;
         this.service = service;
+    }
+
+    @PostMapping
+    public User save(@RequestBody User user) {
+        return userRepository.save(user);
     }
 }
